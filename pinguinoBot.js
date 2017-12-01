766477fe2c7368f845bfbc4e572dd6aac52aac95
Raft of Penguins <raftofpenguins@gmail.com>
2017-12-01 13:32:46 -0800

    Setup command branch syntax

--- pinguinoBot.js
+++ pinguinoBot.js
@@ -14,16 +14,31 @@ const Discord = require("discord.js");
 const client = new Discord.Client(); 		// represents <Client> class; some call it bot
 const config = require("./config.json"); 	// pairs config file to bot
 
-client.login(config.token);
+client.login(config.token);					// Logs the bot in using token from the config file
 
 client.on("ready", () => {
-  console.log("I am ready!");
+	console.log("I am ready!");
 });
 
+
+// Begin handling for how bot handles messages
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+	
+	// If msg from bot or not beginning with prefix, skip
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return; 
+
+	// Trims command into array of arguments
+	// Returns command in all lowercase letters
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g); 	
+	const command = args.shift().toLowerCase();
+
+
+	if(command === 'ping') {
+		message.channel.send('Pong!');
+	}
+
+
+
 });
 
 
